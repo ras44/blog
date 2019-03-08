d4e9ba63818262f3791b7d7215ef7b285cddeea5
Roland Stevenson <roland@rmg-services.com>
2019-03-08 11:26:13 +0000

    Update the blog

--- push.sh
+++ push.sh
@@ -5,6 +5,6 @@ set -e
 find . | grep -vE '(.git|.Rproj.user|public|push.sh)' | xargs rm -rf || true
 
 cp -r public/* ./
-git add . 
+git add -A . 
 git commit -m "Update the blog" 
 git push origin HEAD
